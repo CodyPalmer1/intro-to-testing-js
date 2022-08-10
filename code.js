d69fd959758d6ebc04b2a9501c060e7ef9b196e4
Cody Palmer <codyryanpalmer@gmail.com>
2022-08-10 16:38:56 -0500

    final intro-to-testing

--- code.js
+++ code.js
@@ -14,9 +14,29 @@ function helloWorld() {
 // }
 
 function sayHello(input){
-    if(input){
+    if(typeof input === 'string'){
         return `Hello, ${input}`
     } else {
         return "Hello, World!"
+    }
+}
+
+function isFive(input){
+    return input === 5;
+}
+
+function isEven(input) {
+    return parseInt(input) % 2 === 0;
+}
+
+function isVowel(input){
+    return input === 'a' || input === 'e' || input === 'i' || input === 'o' || input === 'u' || input === 'A' || input === 'E' || input === 'I' || input === 'O' || input === 'U';
+}
+
+function add(input1, input2){
+    if(input1 + input2){
+        return parseInt(input1) + parseInt(input2);
+    } else {
+        return NaN
     }
 }
